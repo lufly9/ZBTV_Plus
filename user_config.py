778df7bb94fe91b707caab4f22dde443b73cb3fe
lufly9 <126124578+lufly9@users.noreply.github.com>
2024-12-02 03:57:10 +0800

    Update user_config.py

--- user_config.py
+++ user_config.py
@@ -1,9 +1,20 @@
+# docker中不要修改
 source_file = "demo.txt"
 final_file = "result.txt"
-zb_urls_limit = 5
+
+# 权重，用于测速，一般不需要修改
 response_time_weight = 0.5
 resolution_weight = 0.5
+
+# 每个频道直播源数量
+zb_urls_limit = 5
+
+# 最大协程数
+max_concurrent_tasks = 10
+
+# 是否打开测速
 open_sort = True
+# 是否找不到源时保留demo的源
 is_use_demo_if_none = False
 # 1: 显示默认值，线路1 线路2，2：显示视频分辨率，如:1080x720
 xianlu_type = 1
@@ -12,16 +23,16 @@ ffmpeg_time = 10
 # key: 地区，在http://tonkiang.us网站上搜索的关键词
 # value: 订阅url，在https://github.com/xisohi/IPTV-Multicast-source中找自己想要的
 search_dict = {
-    "四川": "https://raw.githubusercontent.com/lufly9/Y/refs/heads/main/四川电信订阅.txt",
+    "四川省": "https://raw.githubusercontent.com/lufly9/Y/refs/heads/main/四川电信订阅.txt"
 }
 # 在http://tonkiang.us网站上搜索的源的页数
 search_page_num = 5
 # url关键词黑名单
 url_keywords_blacklist = []
 # 忽略的关键词，比如在demo.txt中配置广东珠江,但在订阅中只有广东珠江高清,就需要忽略掉"高清"
-search_ignore_key = ["HD"]
+search_ignore_key = ["高清"]
 # crawl_type的默认值为1-只爬取http://tonkiang.us上组播源；2-只爬取crawl_urls中配置的网站；3-全部
-crawl_type = "3"
+crawl_type = "1"
 # 收集其他大佬url中的直播源
 crawl_urls = [
     "https://raw.githubusercontent.com/lufly9/Y/refs/heads/main/轮播.txt"
